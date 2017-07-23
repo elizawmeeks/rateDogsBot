7cda88e1395b76cccb42599d1ee4de5c42289e50
Eliza Meeks <theartstudio@Elizas-MacBook-Pro.local>
2017-07-22 20:50:43 -0500

    SORTED THE TWEETS COMMIT COMMIT COMMIT

--- js/getTweets.js
+++ js/getTweets.js
@@ -1,21 +1,56 @@
 "use strict";
 
-console.log("hey baby");
+let Twit = require('../lib/node_modules/twit'),
+	config = require("./config.js"),
+	T = new Twit(config);
 
-let random = require('./random.js'),
-	convert = require('./convert.js'),
-	postPic = require("./post.js"),
-	getTweets = require("./rateDogs/getTweets.js");
+let getTweets = () => {
+	let tweetsArray = [],
+		sentenceArray = [],
+		ratingArray=[];
+	return new Promise((resolve, reject) => {
 
-Promise.all([random.getName(), random.getDog(), getTweets()])
-.then(values => {
-	let name = values[0].Name,
-		pronoun = values[0].Pronoun,
-		sentence = values[2][0],
-		rating = values[2][1];
-	console.log(name);
-	console.log(pronoun);
-	convert(values[1]).then(data=>{
-		postPic(data, name, sentence, rating);
+		// Getting we rate dogs tweets
+		T.get('statuses/user_timeline', {screen_name: "dog_rates", count: 30, include_rts: false, exclude_replies: true}, (err, data, response) => {
+			if (err){
+				console.log(err);
+			}else{
+				// console.log(data);
+				data.forEach(thing => {
+					// console.log(thing.text);
+					let tweet = thing.text,
+							tempSentArray = [],
+							tempRatingArray = [];
+					let tempArray = tweet.split(" ");
+					if (tempArray[0] === "This" && tempArray[1] === "is"){
+						tempArray.splice(0, 3);
+						let filterArray4Sentences = (string) => {
+							if (!string.includes("http") && !string.includes("/10"))
+							{
+								return string;
+							}
+						};
+						let filterRates = (string) => {
+							if (string.includes("/10")){
+								ratingArray.push(string);
+							}
+						};
+						tempSentArray = tempArray.filter(filterArray4Sentences);
+						tempRatingArray = tempArray.filter(filterRates);
+
+						tweetsArray.push(tempSentArray.join(" "));
+					}
+				});
+				let randomSentNo = Math.floor(Math.random()*(tweetsArray.length + 1)),
+						randomRateNo = Math.floor(Math.random()*(tweetsArray.length + 1));
+				console.log(tweetsArray[randomSentNo]);
+				console.log(ratingArray[randomRateNo]);
+				resolve(tweetsArray[randomSentNo], ratingArray[randomRateNo]);
+			}
+		});
 	});
-});
+};
+
+getTweets();
+// module.exports = getTweets;
+// Getting we rate dogs tweets
